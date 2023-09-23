330335b86c5507ddec31921f79a033dcdaf5ef9b
AutoPilotCoder <Haodeveloper3@gmail.com>
2023-09-23 23:24:19 +0800

    query page-[added textbox, can now save value]

--- app/src/main/java/com/example/fit3162_app/MainActivity.java
+++ app/src/main/java/com/example/fit3162_app/MainActivity.java
@@ -4,9 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String query_input;
+
+    EditText input_multiline_view;
+
+    Button input_query_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +29,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Input_query_search(){
-        TextView input_multiline_view=findViewById(R.id.Input_query_text);
+        input_multiline_view=(EditText) findViewById(R.id.Input_query_text);
         input_multiline_view.setMovementMethod(new ScrollingMovementMethod());
+
+        input_query_button=(Button) findViewById(R.id.Input_query_button);
+        input_query_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                query_input=input_multiline_view.getText().toString();
+
+                showToast(query_input);
+            }
+        });
+    }
+
+    public void showToast(String toast_text){
+        Toast.makeText(MainActivity.this, toast_text, Toast.LENGTH_SHORT).show();
     }
 }
